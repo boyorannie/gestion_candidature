df4e90bcb29bc3cb59376f052868e552ee03a2c1
Boyor Annie Thérèse BADIANE <boyorthrse@gmail.com>
2023-12-20 14:13:50 +0000

    documentation/swagger

--- candidature/app/Http/Controllers/CandidatureController.php
+++ candidature/app/Http/Controllers/CandidatureController.php
@@ -8,7 +8,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreCandidatureRequest;
 use App\Http\Requests\UpdateCandidatureRequest;
-
 class CandidatureController extends Controller
 {
 
